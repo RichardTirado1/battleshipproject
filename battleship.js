de7812ff1227cff3e5314670bede58512ebd88a9
Richard Tirado <richardtirado1@yahoo.com>
2016-09-08 09:18:53 -0700

    Adding color to text

--- battleship.js
+++ battleship.js
@@ -34,6 +34,7 @@ for (i = 0; i < cols; i++) {
     // give each div element a unique id based on its row and column, like "s00"
 		square.id = 's' + j + i;
 		square.className = "boardSquare";
+	
 
 		// THIS IS WHERE YOU WILL ADD CODE FOR PART 1 TO ADD TEXT TO EACH SQUARE
   //var letterArray = [j];
